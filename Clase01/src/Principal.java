84efc7ff1980355522c6bd1a951ff52e4ecf113f
Octavio Robleto <octavio.robleto@gmail.com>
2022-05-27 22:05:59 -0300

    Clase01 yClase 02 -> hasta Scanner

---
+++ Clase01/src/Principal.java
@@ -0,0 +1,37 @@
+
+public class Principal {
+
+// este metodo es para inicializar el archivo o programa
+	public static void main(String[] args) {
+		// constante
+		final byte miByte;
+		miByte = -128;
+		short miShort = 32000;
+		int edad1 = 5;
+		int edad2 = 6;
+		int edad3 = 8;
+		int edad4, edad5 = 8, edad6 = 9;
+		long numero = 5555555555L;
+		
+		System.out.println(miByte);
+		
+		
+		System.out.println(miByte);
+		
+		float sueldo = 5.369999F; // hardcodeo
+		double sueldoGerente = 56.33333333333D;
+		
+		System.out.println(sueldo);
+		
+		
+		boolean activo = true; // false
+		
+		char caracter = '"' ;
+		
+		String nombre = "Octavio' Robleto";
+		
+		System.out.println(nombre);
+		
+		
+	}
+}
