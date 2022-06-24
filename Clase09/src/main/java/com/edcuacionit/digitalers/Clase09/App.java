3aaab7debb9e27190f84a62bf410c53b3544dbeb
Octavio Robleto <octavio.robleto@gmail.com>
2022-06-24 19:18:34 -0300

    Inicio clase 08

---
+++ Clase09/src/main/java/com/edcuacionit/digitalers/Clase09/App.java
@@ -0,0 +1,13 @@
+package com.edcuacionit.digitalers.Clase09;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
---
+++ Clase09/src/main/java/com/edcuacionit/digitalers/entidades/Alumno.java
@@ -0,0 +1,8 @@
+package com.edcuacionit.digitalers.entidades;
+
+// una clase solo acepta modificador de acceso default public 
+public class Alumno {  
+	
+	
+	
+}
